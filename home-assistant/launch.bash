1a39e2763fa17e9b445e7e7e7e1bc1714564a81a
Richard Compton <rcompton78@gmail.com>
2026-06-07 18:20:43 -0600

    feat: extract credentials to .env files, add .env.example for all stacks

--- home-assistant/launch.bash
+++ home-assistant/launch.bash
@@ -5,25 +5,20 @@ usage() {
     exit 0
 }
 
-UUID=`id -u`
-GUID=`id -u`
-
 if [ "$1" = "start" ]; then
-    docker-compose --env-file ha.env up -d
+    docker-compose up -d
 elif [ "$1" = "debug" ]; then
-    docker-compose --env-file ha.env up 
+    docker-compose up
 elif [ "$1" = "stop" ]; then
-    docker-compose --env-file ha.env down
+    docker-compose down
 elif [ "$1" = "restart" ]; then
-    docker-compose --env-file ha.env down && docker-compose --env-file ha.env up -d 
+    docker-compose down && docker-compose up -d
 elif [ "$1" = "update" ]; then
     git fetch && \
     git pull origin master && \
     docker-compose pull && \
-    docker-compose --env-file ha.env down && \
-    docker-compose --env-file ha.env up -d 
+    docker-compose down && \
+    docker-compose up -d
 else
     usage
 fi
-
-
